dc93f60abf189d74a2288251a7736d3b6e11ba29
Mohirufy <mohamadirsyad2004@gmail.com>
2025-08-14 02:10:04 +0700

    otakudesu

---
+++ Otakudesu/src/main/kotlin/com/Otakudesu/OtakudesuPlugin.kt
@@ -0,0 +1,11 @@
+package com.Otakudesu
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class OtakudesuPlugin: BasePlugin() {
+    override fun load() {
+        registerMainAPI(Otakudesu())
+    }
+}
